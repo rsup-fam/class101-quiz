0f9f90b031211f2837b5c55a25b6a2fef4830ac1
byungi <tiniwz@gmail.com>
2020-02-10 17:02:01 +0900

    파일합치기

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-import Vehicle from './Vehicle';
+class Vehicle {
+  private numberOfWheels: number; // bigger than -1
+  public wheels: Wheel[];
+  private fuel: number; // 0~100
+
+  constructor(numberOfWheels: number, wheels: Wheel[], fuel: number) {
+    this.numberOfWheels = numberOfWheels;
+    this.wheels = wheels;
+    this.fuel = fuel;
+  }
+  public getNumberOfWheels() { return this.numberOfWheels; }
+
+  public getFuel() {return this.fuel; }
+}
+
 
 export class Wheel {
   public kind: string;
